5da76b23d1d2ff7bcd48439af8786d292c5ec3e2
Negar-Arianmehr <negar.arianmehr89@gmail.com>
2022-02-16 18:10:25 +0100

    last commit

--- js/plugins.js
+++ js/plugins.js
@@ -1,9 +1,31 @@
 var button = document.querySelectorAll(".btn")
-
-button.forEach(btn => btn.addEventListener("click", function (e) {
-    document.querySelector("#myModal").insertAdjacentHTML("beforebegin",
-        "<div><h1>Thank you for visiting my website.</h1></div>")
-}))
+var modalDiv = document.querySelector("#myModal")
+button.forEach(function (btn) {
+    var modal = function () {
+        btn.addEventListener("click", function (e) {
+            e.preventDefault()
+            document.querySelector("#myModal").classList.remove("noShow")
+            document.querySelector("#myModal").classList.add("modal")
+            console.log('first')
+        })
+        console.log('second')
+    }
+    modal()
+    // clearTimeout(function () {
+    //     modal()
+    // }, 2500)
+})
+modalDiv.addEventListener("click", function () {
+    modalDiv.classList.remove("modal")
+            modalDiv.classList.add("noShow")
+})
+// var child = document.querySelector("#child")
+// console.log(child)
+// if (child) {
+//     setTimeout(function () {
+//         child.remove();
+//     }, 2500)
+// }
 
 /*========== NAVBAR TRANSPARENT TO SOLID ==========*/
 function checkScroll() {
@@ -57,7 +79,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
-
 /*========== BOUNCING DOWN ARROW ==========*/
 $(document).ready(function () {
     $(window).scroll(function () {
@@ -75,7 +96,7 @@ $(document).ready(function () {
 });
 
 /*========== MEET THE TEAM CAROUSEL ==========*/
-$(document).ready(function(){ //when document(DOM) loads completely
+$(document).ready(function () { //when document(DOM) loads completely
     $('#team-carousel').owlCarousel({ //owlCarousel settings
         autoplay: true, //set to false to turn off autoplay and only use nav
         autoplayHoverPause: true, //set to false to prevent pausing on hover
@@ -83,19 +104,19 @@ $(document).ready(function(){ //when document(DOM) loads completely
         autoplayTimeout: 8000, //time between transitions
         smartSpeed: 1200, //transition speed
         navSpeed: 1000, //transition speed when using dots/buttons
-        responsive : { //set number of items shown per screen width
-            0 : {
+        responsive: { //set number of items shown per screen width
+            0: {
                 items: 1 //0px width and up display 1 item
             },
-            768 : {
+            768: {
                 items: 2 //576px width and up display 2 items
             },
-            992 : {
+            992: {
                 items: 3 //768px width and up display 3 items
             }
         }
     });
-  });
+});
 
 
 /*========== SKILLS COUNTER ==========*/
@@ -109,7 +130,7 @@ $(document).ready(function () {
 
 /*========== About us CAROUSEL ==========*/
 
-$(document).ready(function(){ //when document(DOM) loads completely
+$(document).ready(function () { //when document(DOM) loads completely
     $('#About_us-carousel').owlCarousel({ //owlCarousel settings
         autoplay: true, //set to false to turn off autoplay and only use nav
         autoplayHoverPause: true, //set to false to prevent pausing on hover
@@ -117,16 +138,16 @@ $(document).ready(function(){ //when document(DOM) loads completely
         autoplayTimeout: 8000, //time between transitions
         smartSpeed: 1600, //transition speed
         navSpeed: 1000, //transition speed when using dots/buttons
-        responsive : { //set number of items shown per screen width
-            0 : {
+        responsive: { //set number of items shown per screen width
+            0: {
                 items: 1 //0px width and up display 1 item
             },
-            768 : {
+            768: {
                 items: 2 //576px width and up display 2 items
             },
         }
     });
-  });
+});
 
 /*========== TOP SCROLL BUTTON ==========*/
 
@@ -137,31 +158,31 @@ $(document).ready(function(){ //when document(DOM) loads completely
 /*========== WAYPOINTS ANIMATION DELAY ==========*/
 //Original Resource: https://www.oxygenna.com/tutorials/scroll-animations-using-waypoints-js-animate-css
 $(function () { // a self calling function
-  function onScrollInit(items, trigger) { // a custom made function
-      items.each(function () { //for every element in items run function
-          var osElement = $(this), //set osElement to the current
-              osAnimationClass = osElement.attr('data-animation'), //get value of attribute data-animation type
-              osAnimationDelay = osElement.attr('data-delay'); //get value of attribute data-delay time
+    function onScrollInit(items, trigger) { // a custom made function
+        items.each(function () { //for every element in items run function
+            var osElement = $(this), //set osElement to the current
+                osAnimationClass = osElement.attr('data-animation'), //get value of attribute data-animation type
+                osAnimationDelay = osElement.attr('data-delay'); //get value of attribute data-delay time
 
-          osElement.css({ //change css of element
-              '-webkit-animation-delay': osAnimationDelay, //for safari browsers
-              '-moz-animation-delay': osAnimationDelay, //for mozilla browsers
-              'animation-delay': osAnimationDelay //normal
-          });
+            osElement.css({ //change css of element
+                '-webkit-animation-delay': osAnimationDelay, //for safari browsers
+                '-moz-animation-delay': osAnimationDelay, //for mozilla browsers
+                'animation-delay': osAnimationDelay //normal
+            });
 
-          var osTrigger = (trigger) ? trigger : osElement; //if trigger is present, set it to osTrigger. Else set osElement to osTrigger
+            var osTrigger = (trigger) ? trigger : osElement; //if trigger is present, set it to osTrigger. Else set osElement to osTrigger
 
-          osTrigger.waypoint(function () { //scroll upwards and downwards
-              osElement.addClass('animated').addClass(osAnimationClass); //add animated and the data-animation class to the element.
-          }, {
-                  triggerOnce: true, //only once this animation should happen
-                  offset: '70%' // animation should happen when the element is 70% below from the top of the browser window
-              });
-      });
-  }
+            osTrigger.waypoint(function () { //scroll upwards and downwards
+                osElement.addClass('animated').addClass(osAnimationClass); //add animated and the data-animation class to the element.
+            }, {
+                triggerOnce: true, //only once this animation should happen
+                offset: '70%' // animation should happen when the element is 70% below from the top of the browser window
+            });
+        });
+    }
 
-  onScrollInit($('.os-animation')); //function call with only items
-  onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
+    onScrollInit($('.os-animation')); //function call with only items
+    onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
 });
 
 
@@ -169,45 +190,45 @@ $(function () { // a self calling function
 //Original Resource: https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
 $(function () {
 
-  // init the validator
-  // validator files are included in the download package
-  // otherwise download from http://1000hz.github.io/bootstrap-validator
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
 
-  $('#contact-form').validator();
+    $('#contact-form').validator();
 
 
-  // when the form is submitted
-  $('#contact-form').on('submit', function (e) {
+    // when the form is submitted
+    $('#contact-form').on('submit', function (e) {
 
-      // if the validator does not prevent form submit
-      if (!e.isDefaultPrevented()) {
-          var url = "contact/contact.php";
+        // if the validator does not prevent form submit
+        if (!e.isDefaultPrevented()) {
+            var url = "contact/contact.php";
 
-          // POST values in the background the the script URL
-          $.ajax({
-              type: "POST",
-              url: url,
-              data: $(this).serialize(),
-              success: function (data) {
-                  // data = JSON object that contact.php returns
+            // POST values in the background the the script URL
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data) {
+                    // data = JSON object that contact.php returns
 
-                  // we recieve the type of the message: success x danger and apply it to the
-                  var messageAlert = 'alert-' + data.type;
-                  var messageText = data.message;
+                    // we recieve the type of the message: success x danger and apply it to the
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
 
-                  // let's compose Bootstrap alert box HTML
-                  var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    // let's compose Bootstrap alert box HTML
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
 
-                  // If we have messageAlert and messageText
-                  if (messageAlert && messageText) {
-                      // inject the alert to .messages div in our form
-                      $('#contact-form').find('.messages').html(alertBox);
-                      // empty the form
-                      $('#contact-form')[0].reset();
-                  }
-              }
-          });
-          return false;
-      }
-  })
+                    // If we have messageAlert and messageText
+                    if (messageAlert && messageText) {
+                        // inject the alert to .messages div in our form
+                        $('#contact-form').find('.messages').html(alertBox);
+                        // empty the form
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
 });
